4c5b045a58483a37d281d9b3fc37ebf4cd2d0f19
peppe-p <ombrachiara96@gmail.com>
2021-04-29 16:19:00 +0200

    Completed Pari e Dispari

--- main.js
+++ main.js
@@ -5,21 +5,45 @@
 
 //Pari e Dispari:
 //L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-var scelta_user = prompt("Scegli fra 'Pari' e 'Dispari'").toLowerCase;
+var scelta_user = prompt("Scegli fra 'Pari' e 'Dispari'").toLowerCase();
 var num_user = parseInt(prompt("Scegli un numero da 1 a 5"));
 
 //Generiamo un numero random(sempre da 1 a 5) per il computer(usando una funzione).
 /**
- * Genera un numero da 1 a X
+ * Genera un numero da 1 a X.
  * @param {Number} x
  * @returns {Number}
  */
-function randomNum(x) {
+function randomNumFrom(x) {
     num_generated = parseInt(Math.random() * x) + 1;
     return num_generated;
 }
-var num_pc = randomNum(5);
-console.log(num_pc);
+var num_pc = randomNumFrom(5);
+
 
 //Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari(usando una funzione)
+/**
+ * Somma 2 numeri e dichiara se il risultato è pari o dispari.
+ * @param {Number} x 
+ * @param {Number} y 
+ * @returns {String}
+ */
+function sumAndPariDispari(x, y) {
+    var somma = x + y;
+    if (somma % 2 == 0) {
+        return "pari";
+    } else {
+        return "dispari";
+    }
+}
+var num_finale = sumAndPariDispari(num_user, num_pc);
+console.log("Numero pc: " + num_pc);
+console.log("Tua scelta: " + num_user + " " + scelta_user);
+console.log("Risultato finale: " + num_finale);
+
 //Dichiariamo chi ha vinto.
+if (num_finale == scelta_user) {
+    console.log("Hai vinto! Congratulazioni!!");
+} else {
+    console.log("Mi dispiace, hai perso...");
+}
